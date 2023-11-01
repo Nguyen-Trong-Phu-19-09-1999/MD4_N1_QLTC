a4496c10ab45af156ad310efccf8d9b6cbb0364d
trananh195 <trananhtnu195@gmail.com>
2023-11-01 11:13:09 +0700

    xong crud user

--- src/main/java/com/example/casemd4/repository/IUserRepository.java
+++ src/main/java/com/example/casemd4/repository/IUserRepository.java
@@ -5,6 +5,9 @@ import com.example.casemd4.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
 }
